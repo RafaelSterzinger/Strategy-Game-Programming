cc2b991d8ac13b1fc3e3fa6feb79ed6cadca7ff9
rafael <rafael.sterzinger@gmx.at>
2020-01-11 23:46:37 +0100

    update model

--- src/main/java/at/pwd/model/Model.java
+++ src/main/java/at/pwd/model/Model.java
@@ -1,14 +1,24 @@
 package at.pwd.model;
 
+import at.pwd.game.State;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.ConvolutionMode;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.graph.MergeVertex;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.io.ClassPathResource;
 import org.nd4j.linalg.learning.config.Sgd;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.IOException;
 
 public class Model {
 
@@ -19,20 +29,33 @@ public class Model {
     private static final int VALUE_FC_SIZE = 256;
     private static final String RESIDUAL_NAME = "Res";
 
-    private ComputationGraphConfiguration model;
+    private ComputationGraph model;
+
+    public INDArray[] predict(State state){
+        return model.output(state.getStateForModel());
+    }
 
     public Model() {
+        model = new ComputationGraph(createConfig());
+        model.init();
+    }
+
+    private ComputationGraphConfiguration createConfig() {
         ComputationGraphConfiguration.GraphBuilder builder = new NeuralNetConfiguration.Builder()
                 .updater(new Sgd(0.01)) // SGD(lr=1e-2, momentum=0.9)
                 .graphBuilder()
                 .addInputs("Own", "Enemy")
+                // .setInputTypes(InputType.inf)
                 .addLayer("Conv1", new Convolution1D.Builder()
+                        .nIn(14)
                         .nOut(CNN_FILTER_NUM)
                         .kernelSize(CNN_FILTER_SIZE)
                         .convolutionMode(ConvolutionMode.Same)
                         .l2(L2_REG)
                         .build(), "Own", "Enemy")
                 .addLayer("BatchNorm1", new BatchNormalization.Builder()
+                        .nIn(CNN_FILTER_NUM)
+                        .nOut(CNN_FILTER_NUM)
                         .build(), "Conv1")
                 .addLayer("Activation1", new ActivationLayer(Activation.RELU), "BatchNorm1");
 
@@ -44,126 +67,65 @@ public class Model {
             }
         }
 
-        builder.addLayer("ResOut", new OutputLayer(), "Activation2_" + RESIDUAL_NAME + (RES_LAYER_NUM - 1));
+        builder.addLayer("ResOut", new OutputLayer.Builder().nIn(CNN_FILTER_NUM*2).nOut(CNN_FILTER_NUM*2).build(), "Activation2_" + RESIDUAL_NAME + (RES_LAYER_NUM - 1));
 
         // Policy Head
         builder.addLayer("ConvPol", new Convolution1D.Builder()
+                .nIn(CNN_FILTER_NUM*2)
                 .nOut(2)
                 .kernelSize(1)
                 .l2(L2_REG)
                 .build(), "ResOut")
-                .addLayer("BatchNormPol", new BatchNormalization.Builder()
+                .addLayer("BatchNormPol", new BatchNormalization.Builder().nIn(2).nOut(2)
                         .build(), "ConvPol")
                 .addLayer("ActivationPol", new ActivationLayer(Activation.RELU), "BatchNormPol")
-                .addLayer("DensePol", new DenseLayer.Builder().nOut(6).l2(L2_REG).activation(Activation.SOFTMAX).build(), new CnnToFeedForwardPreProcessor(), "ActivationPol")
-                .addLayer("PolicyOut", new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).build(), "DensePol");
+                .addLayer("DensePol", new DenseLayer.Builder().nIn(2).nOut(6).l2(L2_REG).activation(Activation.SOFTMAX).build(), new CnnToFeedForwardPreProcessor(), "ActivationPol")
+                .addLayer("PolicyOut", new OutputLayer.Builder().nIn(6).nOut(6).lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY).build(), "DensePol");
 
         // Value Head
         builder.addLayer("ConvVal", new Convolution1D.Builder()
+                .nIn(CNN_FILTER_NUM*2)
                 .nOut(1)
                 .kernelSize(1)
                 .l2(L2_REG)
                 .build(), "ResOut")
-                .addLayer("BatchNormVal", new BatchNormalization.Builder()
+                .addLayer("BatchNormVal", new BatchNormalization.Builder().nIn(1).nOut(1)
                         .build(), "ConvVal")
                 .addLayer("ActivationVal", new ActivationLayer(Activation.RELU), "BatchNormVal")
-                .addLayer("DenseVal1", new DenseLayer.Builder().nOut(VALUE_FC_SIZE).l2(L2_REG).activation(Activation.RELU).build(),"ActivationVal")
-                .addLayer("DenseVal2", new DenseLayer.Builder().nOut(1).l2(L2_REG).activation(Activation.TANH).build(), new CnnToFeedForwardPreProcessor(), "DenseVal1")
-                .addLayer("ValueOut", new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY).build(), "DenseVal2");
+                .addLayer("DenseVal1", new DenseLayer.Builder().nIn(1).nOut(VALUE_FC_SIZE).l2(L2_REG).activation(Activation.RELU).build(),"ActivationVal")
+                .addLayer("DenseVal2", new DenseLayer.Builder().nIn(VALUE_FC_SIZE).nOut(1).l2(L2_REG).activation(Activation.TANH).build(), new CnnToFeedForwardPreProcessor(), "DenseVal1")
+                .addLayer("ValueOut", new OutputLayer.Builder().nIn(1).nOut(1).lossFunction(LossFunctions.LossFunction.MSE).build(), "DenseVal2");
 
-        this.model = builder.setOutputs("PolicyOut", "ValueOut").build();
+        return builder.setOutputs("PolicyOut", "ValueOut").build();
     }
 
     private void buildResidualBlock(ComputationGraphConfiguration.GraphBuilder x, int num, String input) {
         String name = RESIDUAL_NAME + num;
-        x.addLayer("OutputInX_" + name, new OutputLayer(), input);
+        x.addLayer("OutputInX_" + name, new OutputLayer.Builder().nIn(CNN_FILTER_NUM).nOut(CNN_FILTER_NUM).build(), input);
         x.addLayer("Conv1_" + name, new Convolution1D.Builder()
+                .nIn(CNN_FILTER_NUM)
                 .nOut(CNN_FILTER_NUM)
                 .kernelSize(CNN_FILTER_SIZE)
                 .convolutionMode(ConvolutionMode.Same)
                 .l2(L2_REG)
                 .build(), "OutputInX_" + name)
-                .addLayer("BatchNorm1_" + name, new BatchNormalization.Builder()
+                .addLayer("BatchNorm1_" + name, new BatchNormalization.Builder().nIn(CNN_FILTER_NUM).nOut(CNN_FILTER_NUM)
                         .build(), "Conv1_" + name)
                 .addLayer("Activation1_" + name, new ActivationLayer(Activation.RELU), "BatchNorm1_" + name)
                 .addLayer("Conv2_" + name, new Convolution1D.Builder()
+                        .nIn(CNN_FILTER_NUM)
                         .nOut(CNN_FILTER_NUM)
                         .kernelSize(CNN_FILTER_SIZE)
                         .convolutionMode(ConvolutionMode.Same)
                         .l2(L2_REG)
                         .build(), "Activation1_" + name)
-                .addLayer("BatchNorm2_" + name, new BatchNormalization.Builder()
+                .addLayer("BatchNorm2_" + name, new BatchNormalization.Builder().nIn(CNN_FILTER_NUM).nOut(CNN_FILTER_NUM)
                         .build(), "Conv2_" + name)
                 .addInputs()
-                .addLayer("OutputX_" + name, new OutputLayer(), "BatchNorm2_" + name)
+                .addLayer("OutputX_" + name, new OutputLayer.Builder().nIn(CNN_FILTER_NUM).nOut(CNN_FILTER_NUM).build(), "BatchNorm2_" + name)
                 .addVertex("Add_" + name, new MergeVertex(), "OutputInX_" + name, "OutputX_" + name)
                 .addLayer("Activation2_" + name, new ActivationLayer(Activation.RELU), "Add_" + name);
     }
 
-//    private class PolicyLoss implements ILossFunction {
-//
-//        private INDArray scoreArray(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
-//            INDArray output = activationFn.getActivation(preOutput.dup(), true);
-//
-//            INDArray negLabels = labels.dup().mul(-1);
-//            INDArray log = Transforms.log(output.add(1e-07));
-//            negLabels.muli(log);
-//            if (mask != null) {
-//                negLabels.muliColumnVector(mask);
-//            }
-//            return negLabels;
-//        }
-//
-//        @Override
-//        public double computeScore(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask, boolean average) {
-//            INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
-//
-//            double score = scoreArr.sumNumber().doubleValue();
-//
-//            if (average) {
-//                score /= scoreArr.size(0);
-//            }
-//
-//            return score;
-//        }
-//
-//        @Override
-//        public INDArray computeScoreArray(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
-//            INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
-//            return scoreArr.sum(1);
-//        }
-//
-//        @Override
-//        public INDArray computeGradient(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask) {
-//            INDArray output = activationFn.getActivation(preOutput.dup(), true);
-//        /*
-//        //NOTE: There are many ways to do this same set of operations in nd4j
-//        //The following is the most readable for the sake of this example, not necessarily the fastest
-//        //Refer to the Implementation of LossL1 and LossL2 for more efficient ways
-//        */
-//            INDArray yMinusyHat = labels.sub(output);
-//            INDArray dldyhat = yMinusyHat.mul(-2).sub(Transforms.sign(yMinusyHat)); //d(L)/d(yhat) -> this is the line that will change with your loss function
-//
-//            //Everything below remains the same
-//            INDArray dLdPreOut = activationFn.backprop(preOutput.dup(), dldyhat).getFirst();
-//            //multiply with masks, always
-//            if (mask != null) {
-//                dLdPreOut.muliColumnVector(mask);
-//            }
-//
-//            return dLdPreOut;
-//        }
-//
-//        @Override
-//        public Pair<Double, INDArray> computeGradientAndScore(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask, boolean average) {
-//            return new Pair<>(
-//                    computeScore(labels, preOutput, activationFn, mask, average),
-//                    computeGradient(labels, preOutput, activationFn, mask));
-//        }
-//
-//        @Override
-//        public String name() {
-//            return "PolicyLoss";
-//        }
-//    }
+
 }
